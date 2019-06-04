2f87aabd39f835fd4fd0184d91603ba86a273509
Sarkis <sarkis_aghazarian@yahoo.com>
2019-06-03 17:26:02 -0700

    completed merge linked lists

--- 401-code-challenges/LL-Merge/__test__/llmerge.test.js
+++ 401-code-challenges/LL-Merge/__test__/llmerge.test.js
@@ -3,20 +3,22 @@
 // Sarkis - Import constructor properties/methods
 const LlMerge = require('../llmerge');
 
-function Node(value, next = null) {
-    this.value = value;
-    this.next = next;
-};
+const linkedList1= new LinkedList();
+linkedList1.insertAtHead(3);
+linkedList1.insertAtHead(2);
+linkedList1.insertAtHead(1);
+const linkedList2= new LinkedList ();
+linkedList2.insertAtHead('c');
+linkedList2.insertAtHead('b');
+linkedList2.insertAtHead('a');
 
 
-describe('Check For Linked List Node Value', () => {
-    it('Can successfully return k node value', () => {
-        let list = new LinkedList();
-        list.append('a');
-        list.append('b');
-        list.append('c');
-        list.append('d');
-        expect(list.returnNodeValue(2)).toEqual('c');
-        expect(list.length).toEqual(5);
-    });
-});
+describe('test if lists merge', () => {
+    test('testing that method zips two lists together', () => {
+        let mergedList = zipper(linkedList1, linkedList2);
+
+        expect(mergedList.value).toEqual(1);
+        expect(mergedList.next.value).toEqual('a');
+        expect(mergedList.next.next.value).toEqual(2);
+    })
+})
